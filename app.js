0d2e05fd40043496a64b74339f0e5e462a3b3b4f
eme-web <udobongeme4@gmail.com>
2024-01-11 12:22:02 +0100

    Fix views path in app

--- app.js
+++ app.js
@@ -29,9 +29,8 @@ app.engine('handlebars', engine({
     allowProtoPropertiesByDefault: true
 }));
 
+app.set('views',  path.join(__dirname, './views'));
 app.set('view engine', 'handlebars')
-
-app.set('views', './views');
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Body parser middleware
